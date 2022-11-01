a52bfaf9db1d0918f6b3e1676c86fcf6e3e3e3e6
thestsaikat <thestsaikat@gmail.com>
2022-11-02 02:27:53 +0600

    select video fragment added

--- app/src/main/java/com/simplerapps/videotoaudio/MainActivity.kt
+++ app/src/main/java/com/simplerapps/videotoaudio/MainActivity.kt
@@ -2,11 +2,31 @@ package com.simplerapps.videotoaudio
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.commit
+import com.simplerapps.videotoaudio.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var viewBinding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(viewBinding.root)
+
+        showSelectVideoFragment()
+    }
+
+    fun showSelectVideoFragment() {
+        val selectVideoFragment = SelectVideoFragment()
+        showFragment(selectVideoFragment)
+    }
+
+    private fun showFragment(fragment: Fragment) {
+        supportFragmentManager.commit {
+            setReorderingAllowed(true)
+            replace(viewBinding.fragmentContainer.id,fragment)
+        }
     }
 }
---
+++ app/src/main/java/com/simplerapps/videotoaudio/SelectVideoFragment.kt
@@ -0,0 +1,7 @@
+package com.simplerapps.videotoaudio
+
+import androidx.fragment.app.Fragment
+
+class SelectVideoFragment : Fragment(R.layout.fragment_select_video) {
+
+}
